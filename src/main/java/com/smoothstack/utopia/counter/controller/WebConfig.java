58f7c2b209bf863d5a0ef08b14282901778094a9
Achavez97 <achavz97@berkeley.edu>
2020-05-14 12:31:30 -0400

    global cors config

--- src/main/java/com/smoothstack/utopia/counter/controller/WebConfig.java
+++ src/main/java/com/smoothstack/utopia/counter/controller/WebConfig.java
@@ -1,18 +1,25 @@
 package com.smoothstack.utopia.counter.controller;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
  
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-    	// Allow these origins on all routes.
-        registry.addMapping("/**").allowedOrigins("http://utopia-airlines.s3-website-us-east-1.amazonaws.com",
-        		"http://localhost:4200");
-    }
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowedOrigins(Arrays.asList("http://utopia-airlines.s3-website-us-east-1.amazonaws.com", 
+	    		"http://localhost:4200"));
+	    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
+	}
+	
 }
